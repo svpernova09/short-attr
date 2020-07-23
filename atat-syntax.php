098f0acb7415a19b9e5c6e4404f1d19c372b2d19
Joe Ferguson <joe@joeferguson.me>
2020-07-23 14:34:12 -0500

    Add code

---
+++ atat-syntax.php
@@ -0,0 +1,16 @@
+<?php
+
+@@ExampleAttribute
+class Foo
+{
+    @@ExampleAttribute
+    public const FOO = 'foo';
+
+    public function foo(@@ExampleAttribute Type $bar) {}
+}
+
+$object = new @@ExampleAttribute class () {};
+
+$f1 = @@ExampleAttribute function () {};
+
+$f2 = @@ExampleAttribute fn() => 1;
